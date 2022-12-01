c5022d57c610e63605d9298939c3961b7959210d
Petr Chatrný <56938867+petrchatrny@users.noreply.github.com>
2022-12-01 13:39:57 +0100

    add frontend of register scene

--- src/main/java/cz/petrchatrny/sopc/controller/RegisterController.java
+++ src/main/java/cz/petrchatrny/sopc/controller/RegisterController.java
@@ -1,4 +1,30 @@
 package cz.petrchatrny.sopc.controller;
 
+import cz.petrchatrny.sopc.App;
+import cz.petrchatrny.sopc.SceneType;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
 public class RegisterController {
+    @FXML
+    private TextField nicknameTF;
+    @FXML
+    private TextField emailTF;
+    @FXML
+    private PasswordField passwordTF;
+    @FXML
+    private PasswordField repeatPasswordTF;
+
+    @FXML
+    private void showLoginForm(MouseEvent mouseEvent) {
+        App.showScene(SceneType.LOGIN);
+    }
+
+    @FXML
+    private void register(ActionEvent actionEvent) {
+        // todo
+    }
 }
