7f126bcd9f7439bd21bae757742e48957988008e
Petr Chatrný <56938867+petrchatrny@users.noreply.github.com>
2022-12-01 14:10:06 +0100

    add home scene frontend

--- src/main/java/cz/petrchatrny/sopc/controller/HomeController.java
+++ src/main/java/cz/petrchatrny/sopc/controller/HomeController.java
@@ -1,4 +1,18 @@
 package cz.petrchatrny.sopc.controller;
 
+import cz.petrchatrny.sopc.App;
+import cz.petrchatrny.sopc.SceneType;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class HomeController {
+    @FXML
+    private void onSinglePlayerSelected(ActionEvent actionEvent) {
+        App.showScene(SceneType.GAME);
+    }
+
+    @FXML
+    private void onMultiPlayerSelected(ActionEvent actionEvent) {
+        // TODO
+    }
 }
