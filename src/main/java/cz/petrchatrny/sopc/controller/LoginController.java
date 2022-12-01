f24b34129538702aabb3a69462f36dc61d605ed3
Petr Chatrný <56938867+petrchatrny@users.noreply.github.com>
2022-12-01 13:35:06 +0100

    add frontend of login scene

--- src/main/java/cz/petrchatrny/sopc/controller/LoginController.java
+++ src/main/java/cz/petrchatrny/sopc/controller/LoginController.java
@@ -1,4 +1,26 @@
 package cz.petrchatrny.sopc.controller;
 
+import cz.petrchatrny.sopc.App;
+import cz.petrchatrny.sopc.SceneType;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
 public class LoginController {
+    @FXML
+    private TextField emailTF;
+    @FXML
+    private PasswordField passwordTF;
+
+    @FXML
+    private void showRegistrationForm(MouseEvent mouseEvent) {
+        App.showScene(SceneType.REGISTER);
+    }
+
+    @FXML
+    private void login(ActionEvent actionEvent) {
+        // TODO
+    }
 }
