4f3e315986348f38fdc66256478e850500670bcd
Petr Chatrný <56938867+petrchatrny@users.noreply.github.com>
2023-01-15 19:10:11 +0100

    update settings

--- src/main/java/cz/petrchatrny/sopc/model/Settings.java
+++ src/main/java/cz/petrchatrny/sopc/model/Settings.java
@@ -1,8 +1,8 @@
 package cz.petrchatrny.sopc.model;
 
 public class Settings {
-    public static final int MAX_SPACESHIP_COUNT = 8;
-    public static final int MAX_SPACE_STATION_COUNT = 8;
+    public static final int MAX_SPACESHIP_COUNT = 10;
+    public static final int MAX_SPACE_STATION_COUNT = 10;
 
     public static final int MAX_PROCESS_ORE_PER_TURN_COUNT = 4;
 }
